aa76ff29f0b83aa8a06c04f462b6c94e90a233ed
Heungjun <gmdwns92@gmail.com>
2021-08-08 23:48:20 +0900

    20210808

--- lib/controller/MyAppController.dart
+++ lib/controller/MyAppController.dart
@@ -9,4 +9,10 @@ class MyAppController extends GetxService {
 
   double get menuButtonWidth => screenWidth * 0.8;
   double get menuButtonHeight => screenHeight * 0.06;
+
+  double get quizContainerWidth => screenWidth * 0.8;
+  double get quizContainerHeight => screenHeight * 0.6;
+  double get quizContainerHeightSmall => screenHeight * 0.07;
+
+  double get sizedBoxHeight => screenHeight * 0.02;
 }
--- lib/pages/QuizPage.dart
+++ lib/pages/QuizPage.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:yhj_quiz_flutter/controller/MyAppController.dart';
 
 class QuizPage extends StatelessWidget {
   const QuizPage({Key? key}) : super(key: key);
@@ -7,8 +8,101 @@ class QuizPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       body: Center(
-        child: Container(
-          child: Text('QuizPage'),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Container(
+              width: MyAppController.to.quizContainerWidth,
+              height: MyAppController.to.quizContainerHeightSmall,
+              color: Colors.yellow,
+              child: Center(
+                child: Text(
+                  '문제 1. 테스트 문제 입니다.',
+                  style: TextStyle(fontSize: 15, fontWeight: FontWeight.bold),
+                ),
+              ),
+            ),
+            SizedBox(height: MyAppController.to.sizedBoxHeight),
+            Container(
+              width: MyAppController.to.quizContainerWidth,
+              height: MyAppController.to.quizContainerHeight,
+              // color: Colors.white,
+              decoration: BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.circular(20),
+              ),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.all(12.0),
+                    child: ElevatedButton(
+                      onPressed: () {},
+                      child: Text('1번. 1'),
+                      style: ElevatedButton.styleFrom(
+                        fixedSize: Size(
+                          MyAppController.to.menuButtonWidth,
+                          MyAppController.to.menuButtonHeight,
+                        ),
+                        primary: Colors.white,
+                        shadowColor: Colors.yellow,
+                        elevation: 10,
+                      ),
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.all(12.0),
+                    child: ElevatedButton(
+                      onPressed: () {},
+                      child: Text('2번. 2'),
+                      style: ElevatedButton.styleFrom(
+                        fixedSize: Size(
+                          MyAppController.to.menuButtonWidth,
+                          MyAppController.to.menuButtonHeight,
+                        ),
+                        primary: Colors.yellow,
+                        shadowColor: Colors.yellow,
+                        elevation: 10,
+                      ),
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.all(12.0),
+                    child: ElevatedButton(
+                      onPressed: () {},
+                      child: Text('3번. 1'),
+                      style: ElevatedButton.styleFrom(
+                        fixedSize: Size(
+                          MyAppController.to.menuButtonWidth,
+                          MyAppController.to.menuButtonHeight,
+                        ),
+                        primary: Colors.white,
+                        shadowColor: Colors.yellow,
+                        elevation: 10,
+                      ),
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.all(12.0),
+                    child: ElevatedButton(
+                      onPressed: () {},
+                      child: Text('4번. 1'),
+                      style: ElevatedButton.styleFrom(
+                        fixedSize: Size(
+                          MyAppController.to.menuButtonWidth,
+                          MyAppController.to.menuButtonHeight,
+                        ),
+                        primary: Colors.white,
+                        shadowColor: Colors.yellow,
+                        elevation: 10,
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ],
         ),
       ),
     );
